a7142d94a313a6cc75342c8ba062fdc80fa6018e
Clara Pons <clarapons@macbook-air-de-clara.home>
2022-06-21 11:28:37 +0200

    first commit exercice

---
+++ index.js
@@ -0,0 +1,44 @@
+// 01 - Comparaison
+// const test = 143
+// const bis = 219
+
+// console.log(test > bis)
+// console.log(test < bis)
+// console.log(test >= bis)
+// console.log(test <= bis)
+// console.log(test !== bis)
+// console.log(test === bis)
+
+// 02 - Condition
+// const limit = 50
+// let score = 44
+
+// if (score >= limit){
+//     console.log("Ok good !")
+// } else {
+//     console.log("Oh noooo")
+// }
+
+// 03 - Condition II
+// const password = "azerty"
+
+// if (password.length >= 5){
+//     console.log("The password is secure")
+// }else {
+//     console.log("The password is not secured")
+// }
+
+// 04 - Condition III
+// const limit = 50
+// let score = 64
+// const password = "azerty"
+
+// if ((score >= limit) && (password.length >= 5)){
+//     console.log("Everything is cool !")
+// }else if ((score >= limit) || (password.length >= 5)){
+//     console.log("Something is cool !")
+// }else{
+//     console.log("Nothing is cool !")
+// }
+
+// 
