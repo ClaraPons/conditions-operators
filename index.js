91e1fae70388b16bb58d4ba3c4d0d1992c78b8b0
Clara Pons <clarapons@macbook-air-de-clara.home>
2022-06-21 12:22:14 +0200

    execercies suite

--- index.js
+++ index.js
@@ -41,4 +41,67 @@
 //     console.log("Nothing is cool !")
 // }
 
-// 
+// // 05 - Random
+// const min = 1
+// const max = 6
+
+// const random = Math.floor(Math.random() * max) + min
+
+// if (random === 6){
+//     console.log("Yes, i win !")
+// }else{
+//     console.log("So close...")
+// }
+
+// 06 - Month
+// let month = "Dld,e,mber"
+
+// switch (month) {
+//     case "January": 
+//         console.log("Winter")
+//         break
+//     case "February": 
+//         console.log("Winter")
+//         break
+//     case "March": 
+//         console.log("Spring")
+//         break
+//     case "April": 
+//         console.log("Spring")
+//         break
+//     case "May": 
+//         console.log("Spring")
+//         break
+//     case "June": 
+//         console.log("Summer")
+//         break
+//     case "July": 
+//         console.log("Summer")
+//         break
+//     case "August": 
+//         console.log("Summer")
+//         break
+//     case "September": 
+//         console.log("Fall")
+//         break
+//     case "October": 
+//         console.log("Fall")
+//         break
+//     case "November": 
+//         console.log("Fall")
+//         break
+//     case "December": 
+//         console.log("Winter")
+//         break
+//     default: 
+//         console.log("This isn't a month")
+// }
+
+// 07 - Rounded but better
+let roundedNumber = 3.8
+
+if (roundedNumber < 3.5){
+    console.log(Math.floor(roundedNumber))
+}else if (roundedNumber >= 3.5){
+    console.log(Math.ceil(roundedNumber))
+}
